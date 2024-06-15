00e72354bb316142824c2da1868fc47843d931d8
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-06-15 19:26:12 -0400

    s

--- components/Chat.js
+++ components/Chat.js
@@ -1,18 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+
 
 const Chat = ({ route, navigation }) => {
-// import name and chatBackgroundColor from Start.js
+  const [messages, setMessages] = useState([]);
+  
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
+
+  // import name and chatBackgroundColor from Start.js
   const { name } = route.params;
   const { chatBackgroundColor } = route.params;
 
-// set screen title to the what the user typed in the textInput field in Start.js
+  // set screen title to the what the user typed in the textInput field in Start.js
   useEffect(() => {
     navigation.setOptions({ title: name });
   }, []);
 
   return (
-    <View style={[styles.container, {backgroundColor: chatBackgroundColor}]}>
+    <View style={[styles.container, { backgroundColor: chatBackgroundColor }]}>
       <Text>Hello Chat!</Text>
     </View>
   );
