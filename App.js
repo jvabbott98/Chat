ee4a819de2acf910592f1681c36d2c2db276371b
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-06-19 11:41:00 -0400

    initialized firestore

--- App.js
+++ App.js
@@ -4,10 +4,25 @@ import Start from './components/Start';
 import Chat from './components/Chat';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDyTU56HfsL0L4iLgeatIeaKy0rCes95ig",
+    authDomain: "chat-eff4c.firebaseapp.com",
+    projectId: "chat-eff4c",
+    storageBucket: "chat-eff4c.appspot.com",
+    messagingSenderId: "221605350797",
+    appId: "1:221605350797:web:25fc9f360d0b7fe18581c4"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
       
