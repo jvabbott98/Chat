b6484d5beb6a469d575b8940bc1fdab20f95f5bf
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-06-21 16:56:55 -0400

    Added offline functionality

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, LogBox, Alert } from 'react-native';
 import Start from './components/Start';
 import Chat from './components/Chat';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, disableNetwork, enableNetwork } from "firebase/firestore";
+import { useNetInfo }from '@react-native-community/netinfo';
+import { useEffect } from 'react';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const connectionStatus = useNetInfo();
+  useEffect(() => {
+    if (connectionStatus.isConnected === false) {
+      Alert.alert("Connection Lost!");
+      disableNetwork(db);
+    } else if (connectionStatus.isConnected === true) {
+      enableNetwork(db);
+    }
+  }, [connectionStatus.isConnected]);
 
   const firebaseConfig = {
     apiKey: "AIzaSyDyTU56HfsL0L4iLgeatIeaKy0rCes95ig",
@@ -36,7 +48,7 @@ const App = () => {
         <Stack.Screen
           name="Chat"
         >
-        {props => <Chat db={db} {...props} />}
+        {props => <Chat isConnected={connectionStatus.isConnected} db={db} {...props} />}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
--- components/Chat.js
+++ components/Chat.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
 import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
-import { Bubble, GiftedChat } from 'react-native-gifted-chat';
+import { Bubble, GiftedChat, InputToolbar,  } from 'react-native-gifted-chat';
 import { collection, getDocs, addDoc, onSnapshot } from "firebase/firestore";
 import { query, orderBy } from 'firebase/firestore';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({ route, navigation, db }) => {
+const Chat = ({ route, navigation, db, isConnected }) => {
   const [messages, setMessages] = useState([]);
   // import name and chatBackgroundColor from Start.js
   const { name } = route.params;
@@ -15,43 +16,77 @@ const Chat = ({ route, navigation, db }) => {
     addDoc(collection(db, "messages"), newMessages[0])
   }
 
-  const renderBubble = (props) => {
-    return <Bubble
-    {...props}
-    wrapperStyle={{
-      right: {
-        backgroundColor: "#000"
-      },
-      left: {
-        backgroundColor: "#FFF"
-      }
-    }}
-    />
+  const loadCachedMessages = async () => {
+    const cachedMessages = await AsyncStorage.getItem("messages") || [];
+    setMessages(JSON.parse(cachedMessages));
   }
 
+  const renderBubble = (props) => {
+    // if (isConnected)
+    return <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#000"
+        },
+        left: {
+          backgroundColor: "#fff"
+        }
+      }}
+    />;
+  //   else return <Bubble
+  //   {...props}
+  //   wrapperStyle={{
+  //     right: {
+  //       backgroundColor: "red"
+  //     },
+  //     left: {
+  //       backgroundColor: "red"
+  //     }
+  //   }}
+  // />;
+  }
+
+  let unsubMessages
   useEffect(() => {
     navigation.setOptions({ title: name });
-    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-    const unsubMessages = onSnapshot(q, (docs) => {
-      let newMessages = [];
-      docs.forEach(doc => {
-        newMessages.push({
-          id: doc.id,
-          ...doc.data(),
-          createdAt: new Date(doc.data().createdAt.toMillis())
-        })
+
+    if (isConnected === true) {
+      if (unsubMessages) unsubMessages();
+      unsubMessages = null;
+
+      const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+      unsubMessages = onSnapshot(q, (docs) => {
+        let newMessages = [];
+        docs.forEach(doc => {
+          newMessages.push({
+            id: doc.id,
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis())
+          });
+          cacheMessages(newMessages);
+          setMessages(newMessages);
+        });
       })
-      setMessages(newMessages);
-    })
+    } else loadCachedMessages();
     return () => {
       if (unsubMessages) unsubMessages();
     }
-   }, []);
+  }, [isConnected]);
 
-  // set screen title to the what the user typed in the textInput field in Start.js
-  // useEffect(() => {
-  //   navigation.setOptions({ title: name });
-  // }, []);
+  const cacheMessages = async (messagesToCache) => {
+    try {
+      await AsyncStorage.setItem('messages', JSON.stringify(messagesToCache));
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+ 
+  const renderInputToolbar = (props) => {
+    if (isConnected) return <InputToolbar {...props} />;
+    else return null;
+   }
 
   return (
     <View style={[styles.container, { backgroundColor: chatBackgroundColor }]}>
@@ -59,15 +94,17 @@ const Chat = ({ route, navigation, db }) => {
         style={styles.chat}
         messages={messages}
         renderBubble={renderBubble}
+        renderInputToolbar={renderInputToolbar}
         onSend={messages => onSend(messages)}
         user={{
           _id: userID,
           name: name
         }}
       />
-      { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
-    </View>
+      {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
       
+    </View>
+
 
   );
 }
@@ -81,4 +118,4 @@ const styles = StyleSheet.create({
 
 export default Chat;
 
-{/* <View style={[styles.container, { backgroundColor: chatBackgroundColor }]}> */}
+{/* <View style={[styles.container, { backgroundColor: chatBackgroundColor }]}> */ }
